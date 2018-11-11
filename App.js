d98925d620afa864ac7359656b7612565e3e8807
Grégory Paul <paulgreg@gmail.com>
2018-11-11 21:19:05 +0100

    Implement add/del + messages

--- App.js
+++ App.js
@@ -30,11 +30,8 @@ export default class App extends React.Component {
   }
 
   go () {
-    if (!this.state.feed) {
-      this.setState({error: 'No feed name'})
-      return
-    }
-    this.setState({ error: undefined })
+    if (!this.checkFeedName()) return
+
 
     return fetch(`${config.server}?n=${this.state.feed}&l=100`)
       .then((response) => response.text())
@@ -43,28 +40,53 @@ export default class App extends React.Component {
         const items = rss.items.map((item, idx) => {
           return { ...item, id: `${this.state.feed}-${idx}` }
         })
-        this.setState({items})
+        this.setState({
+          title: rss.title,
+          items
+        })
       })
   }
 
   add () {
+    this.do('add', 'added')
   }
 
   del () {
+    this.do('del', 'deleted')
   }
+
+  do (action, verb) {
+    if (!this.checkFeedName() || !this.checkUrl()) return
+
+    this.setState({ items: [] })
+
+    return fetch(`${config.server}${action}}?n=${this.state.feed}&u=${this.state.url}`)
+    .then(() => {
+      this.showMessage(`URL ${verb} to feed ${this.state.feed}`)
+      this.go()
+    })
+    .catch(e => {
+      this.showError(e)
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={{ ...styles.text, height: 20, fontWeight: 'bold', backgroundColor: 'steelblue'}}></Text>
-        <Text style={{ ...styles.text, height: 60, fontSize: 16, backgroundColor: 'steelblue'}}>rsstodolist - {config.server}</Text>
+        <Text style={{ ...styles.text, height: 20, backgroundColor: 'steelblue'}}></Text>
+        <Text style={{ ...styles.text, height: 60, fontWeight: 'bold', color: 'white', fontSize: 16, backgroundColor: 'steelblue'}}>rsstodolist - {config.server}</Text>
         <TextInput
           style={{height: 40}}
+          autoCapitalize="none"
+          autoFocus={ true }
           placeholder="Feed name"
           value={this.state.feed}
           onChangeText={(feed) => this.setState({feed: feed.toLowerCase()})}
         />
         <TextInput
           style={{height: 40}}
+          autoCapitalize="none"
+          keyboardType="url"
           placeholder="URL"
           onChangeText={(url) => this.setState({url})}
         />
@@ -83,12 +105,62 @@ export default class App extends React.Component {
           title="Delete"
           color="#F63136"
         />
+      {renderIf(this.state.msg,
+        <Text style={{ ...styles.text, height: 60, backgroundColor: 'green'}}>{this.state.msg}</Text>
+      )}
       {renderIf(this.state.error,
         <Text style={{ ...styles.text, height: 60, backgroundColor: 'red'}}>{this.state.error}</Text>
+      )}
+      {renderIf(this.state.title,
+        <Text style={{ ...styles.text, height: 30, backgroundColor: '#CCC'}}>Feed : {this.state.title}</Text>
       )}
         <FeedListView items={ this.state.items } />
       </View>
     )
   }
+
+  checkFeedName () {
+    if (!this.state.feed) {
+      this.showError('no feed name')
+      return false
+    }
+    return true
+  }
+
+  checkUrl () {
+    if (!this.state.url) {
+      this.showError('no URL')
+      return false
+    }
+    const urlRe = /^https?:\/\//
+    if (!urlRe.test(this.state.url)) {
+      this.showError('Bad URL')
+      return false
+    }
+
+    return true
+  }
+
+  showMessage(msg) {
+    this.show('msg', msg)
+  }
+  showError(msg) {
+    this.show('error', msg)
+  }
+
+  show(key, msg) {
+    ['msg', 'error'].map(m => this.emptyMessage(m))
+    this.setState({[key]: msg})
+    this.delay(this.emptyMessage.bind(this, key))
+  }
+
+  emptyMessage(key) {
+    this.setState({ [key]: undefined })
+  }
+
+  delay(fn) {
+    setTimeout(fn, 3500)
+  }
+
 }
 
--- FeedListItem.js
+++ FeedListItem.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, Linking } from 'react-native'
 
 export default class FeedListItem extends React.Component {
 
   render() {
     return (
       <React.Fragment>
-        <Text style={{ fontWeight: 'bold' }} >
-          {this.props.title}
-          <Text style={{ color: 'grey' }} >
-            {this.props.link.url}
-          </Text>
+        <Text style={{ fontWeight: 'bold' }}>
+          {this.props.title} -
+          <Text
+            style={{ color: 'grey' }}
+            onPress={() => Linking.openURL(this.props.link.url)}
+          > {this.props.link.url} </Text>
         </Text>
       </React.Fragment>
     )
