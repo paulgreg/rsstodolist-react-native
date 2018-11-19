928a040c1c0463c611d1a9d4b055dc0fe8688057
Grégory Paul <paulgreg@gmail.com>
2018-11-19 20:57:10 +0100

    Make text and URL « copiable »

--- FeedListItem.js
+++ FeedListItem.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { Text, Linking } from 'react-native'
+import { Text, Linking, Clipboard, ToastAndroid } from 'react-native'
 
 export default class FeedListItem extends React.Component {
+
+  copy (value) {
+    Clipboard.setString(value);
+    ToastAndroid.show('Text copied!', ToastAndroid.SHORT);
+  }
 
   render() {
     return (
       <React.Fragment>
-        <Text style={{ fontWeight: 'bold' }}>
+        <Text style={{ fontWeight: 'bold' }} onLongPress={ this.copy.bind(this, `${this.props.title} ${this.props.link.url}`) }>
           {this.props.title} -
           <Text
             style={{ color: 'grey' }}
             onPress={() => Linking.openURL(this.props.link.url)}
+            onLongPress={ this.copy.bind(this, this.props.link.url) }>
           > {this.props.link.url} </Text>
         </Text>
       </React.Fragment>
