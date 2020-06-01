3faffb0c3c541885807cc761aef5663541012bb8
Grégory Paul <paulgreg@gmail.com>
2020-06-01 19:09:49 +0200

    Starts a new react-native project and paste sources into it...

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,3 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: [
-      "react-native"
-    ],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
 };
